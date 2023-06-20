b659cac6fba79492e111a9e93b43cd9d639a2554
Jeong-wonho <kallzzang@naver.com>
2023-06-20 17:45:07 +0900

    \#2\ foodDiary making

---
+++ models/foodDiary.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const FoodDiarySchema = new Schema({
+    user: {type: ObjectId, ref: 'User', required: true},
+    food: {type : ObjectId, ref: 'Food', required: true},
+    createdAt: {type: Date, default: Date.now},
+    mealsAt: {type: String, required: true},
+    modifiedAt: {type: Date, required: false}
+
+})
